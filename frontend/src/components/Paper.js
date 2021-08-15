fda4e9367fe7b5c8ee412c54d99d8383b41f72e7
izuna385 <izuna385@gmail.com>
2021-08-15 15:37:47 +0900

    単一component Paperを実装。jsonのキーを受け取ることができるように。

--- frontend/src/components/Paper.js
+++ frontend/src/components/Paper.js
@@ -1,11 +1,15 @@
+const Paper = ({ paperData }) => {
+    return (
+        <div className="paper-container">
+            <div className="paper">
+                <h1>{paperData.title}</h1>                   
+                <h4>Abstract: {paperData.summary}</h4>
+                <h4>Task: {paperData.task}</h4>
+                <h4>Unique NE: {paperData.ne}</h4>
+            </div>
 
-const Paper = () => {
+        </div>
+    );
+};
 
-  return (
-    <div>
-      <p>One Paper</p>
-    </div>
-  )
-}
-
-export default Paper
+export default Paper;

f933ae586a042f2032cf721fe8c51632bf0178ae
izuna385 <izuna385@gmail.com>
2021-08-21 18:14:08 +0900

    cssをPaperに適用

--- frontend/src/components/Paper.js
+++ frontend/src/components/Paper.js
@@ -1,13 +1,31 @@
+import Card from 'react-bootstrap/Card';
+import './Paper.css';
+
 const Paper = ({ paperData }) => {
     return (
-        <div className="paper-container">
-            <div className="paper">
-                <h1>{paperData.title}</h1>                   
-                <h4>Abstract: {paperData.summary}</h4>
-                <h4>Task: {paperData.task}</h4>
-                <h4>Unique NE: {paperData.ne}</h4>
-            </div>
-
+        // <div className="paper-container">
+        //     <div className="paper">
+        //         <h1>{paperData.title}</h1>                   
+        //         <h4>Abstract: {paperData.summary}</h4>
+        //         <h4>Task: {paperData.task}</h4>
+        //         <h4>Unique NE: {paperData.ne}</h4>
+        //     </div>
+        // </div>
+        <div class='box7'>
+            <Card>
+                <Card.Body>
+                <Card.Title>{paperData.title}</Card.Title>
+                <Card.Text>
+                    Abstract: {paperData.summary}
+                </Card.Text>
+                <Card.Text>
+                    <small className="text-muted">Task: {paperData.task}</small>
+                </Card.Text>
+                <Card.Text>
+                    <small className="text-muted">Unique NE: {paperData.ne}</small>
+                </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
